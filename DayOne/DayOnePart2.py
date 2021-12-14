7869402092506781bc804cf918bb33381cd3c1fb
Sam Brenton <84792013+sambrenton@users.noreply.github.com>
2021-12-14 14:21:33 +0000

    Create DayOnePart2.py

---
+++ DayOne/DayOnePart2.py
@@ -0,0 +1,14 @@
+numbers = []
+increases = 0 
+
+with open ('input.txt') as input_data:
+    for lines in input_data:
+        numbers.append(int(lines.strip()))
+
+for n in range(0, len(numbers)):
+    current_number = numbers[n]
+    previous_number = numbers[n-3]
+    if current_number > previous_number:
+        increases += 1
+        
+print(increases)
